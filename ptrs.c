cc47908a56dd54c08b16a88cb2c8b7ac1a7a0750
hayk galstyan <hayk.galstyan.pm@gmail.com>
2021-01-07 12:04:01 +0400

    reformat

--- ptrs.c
+++ ptrs.c
@@ -7,14 +7,16 @@ int main() {
 	int intSize = sizeof(int);
 	printf("int size: %d bytes\n", intSize);
 
-	// print int array addresses
-	int arr[10];
-	for(int i = 0 ; i < 10 ; i++) printf("int arr[%d] address: %p\n", i, &arr[i]);
 
 	// print int address
 	int a;
 	int* aptr = &a;
-	printf("aptr %p\n", aptr);
+	printf("int aptr address %p\n", aptr);
+
+
+	// print int array addresses
+	int arr[10];
+	for(int i = 0 ; i < 10 ; i++) printf("int arr[%d] address: %p\n", i, &arr[i]);
 
 
 	return 0;	
